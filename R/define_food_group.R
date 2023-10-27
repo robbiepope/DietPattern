d0fb3f4f1d14a7a9de409f8c0068337ad35edc00
Robbie Pope <robbiepope93@gmail.com>
2023-10-27 15:27:55 +0100

    20231027

--- R/define_food_group.R
+++ R/define_food_group.R
@@ -1,29 +1,28 @@
 ################################################################################
 # FUNCTION: Create a DF with summed meal portions (g) for a food group
-################################################################################ 
-
+################################################################################
 # Function to define the FFQ amalgamated food groups
-define_food_group <- function (df, id_list, food_group_name) 
-  #' Group the DataFrame by ID and calculate the sum of meal_portion for 
+define_food_group <- function (df, id_list, food_group_name)
+  #' Group the DataFrame by ID and calculate the sum of meal_portion for
   #' specified meal_ID codes. Any FFQ IDs with no meal_id data are added as 0g.
   #'
   #' @param df <- FFQ Line FETA processed df of FFQ_IDs, MEAL_IDs and MEAL_Portions
   #' @param id_list <- Vector of numerical meal_ids for a food group
   #' @param food_group_name <- String, name of food group, e.g., "animal_fat"
   #' @return summarized df for food group
-  #' 
+  #'
   #' @author Robbie Pope
-  #'   
+  #'
 {
   # Create df of summed meal portion weights (g) for food group
   summed_df <- df %>%
     group_by(ID) %>%
     summarize(total_meal_portion = sum(ifelse(MEAL_ID %in% id_list, MEAL_PORTION, 0))) %>%
     ungroup()
-  
+
   # Create column with food group name
-  summed_df$FoodGroup <- food_group_name 
-  
+  summed_df$FoodGroup <- food_group_name
+
   return(summed_df) # Return final summed df
 }
 
--- R/define_healthful_group.R
+++ R/define_healthful_group.R
@@ -1,16 +1,15 @@
 ###############################################################################
 # FUNCTION: Define healthful groups
 ################################################################################
-
 # Function to assign FFQ meals into PDI healthful groups
-define_healthful_group <- function (df) 
+define_healthful_group <- function (df)
   #' xx
   #
   #' @param df <- DF with amalgamated FFQ groups in a column called 'FoodGroup'
   #' @return healthful_df <- DF with additional column 'HealthfulGroup' with healthful group assignment
-  #' 
+  #'
   #' @author Robbie Pope
-  #' 
+  #'
 {
   # Define dictionary of healthy, less healthy and animal based foods for PDI scores
   health_groups <- list(
@@ -18,8 +17,8 @@ define_healthful_group <- function (df)
     less_healthy = c('fruit_juice', 'refined_grains', 'potatoes', 'sugar_beverage', 'sweet_dessert'),
     animal_foods = c('animal_fat', 'dairy', 'eggs', 'fish', 'meat', 'misc_animal')
   )
-  
-  # Create additional column assigining each food group to a helathy_group definition 
+
+  # Create additional column assigining each food group to a helathy_group definition
   healthful_df <- df %>%
     mutate(
       HealthfulGroup = case_when(
