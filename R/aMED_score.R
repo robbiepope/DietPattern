149c38645bcdd5085dc9951cab7c071c19197097
Robbie Pope <robbiepope93@gmail.com>
2023-11-27 17:48:20 +0000

    updated amed

--- R/aMED_score.R
+++ R/aMED_score.R
@@ -4,9 +4,13 @@
 # aMED score Calculation Function
 aMED_score <- function(row, median_list)
   #' Comute the aMED score based on the foodgroup median intake.
-  #' Below median intake is scored 0
-  #' Equal to or above intake is scored 1
-  #' For alcohol the score is based on a range
+  #' Fung et al. 2005
+  #' doi.org/10.1093/ajcn/82.1.163
+  #'
+  #' Below median intake is scored 0 for favorable foods
+  #' Equal to or above intake is scored 1 for favorable foods
+  #'
+  #' For alcohol the score is sex specific and based on a range:
   #' Female is scored 1 if between 5-15, otherwise 0
   #' Male is scored 1 if between 5-25, otherwise 0
   #'
